866230a3424380378aef35121a615261fd1d40ab
FOKO KENMOGNE Wilfried <143821047+FOKO-KENMOGNE-Wilfried@users.noreply.github.com>
2024-04-11 21:45:06 +0100

    Fix the conflit

--- briquiny-frontend/src/utils/style/GlobalStyle.jsx
+++ briquiny-frontend/src/utils/style/GlobalStyle.jsx
@@ -7,11 +7,6 @@ const StyledGlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         font-family: "Inter", sans-serif;
     }
-    *::-webkit-scrollbar{
-        width: 0px;
-        height: 0px;
->>>>>>> 4a58c03dd4b2802eb471da92be09d5ee020ea35b
-    }
 `
 
 function GlobalStyle(){
