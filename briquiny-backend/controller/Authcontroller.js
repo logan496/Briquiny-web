86c045bfab70307fe82a780118568e8ae113a23a
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-04-13 21:21:17 +0100

    Corrections des bugs liés au login

--- briquiny-backend/controller/Authcontroller.js
+++ briquiny-backend/controller/Authcontroller.js
@@ -1,25 +1,30 @@
-const AdminModel = require('../models/Admin')
 const jwt = require("jsonwebtoken")
 const bcrypt = require('bcrypt')
+const Auth = require('../middleware/Authentification')
 
 class AuthController{
 
     static async login(req, res){
-        const {name, password, structure} = req.body
-        const User = AdminModel.authenticate(name, password)
-        if (User){
-            res.status(200).json({
-                userId: User._id,
-                token: jwt.sign(
+        try {
+            const {name, password} = req.body
+            const User = await Auth.authenticate(name, password)
+            if (User) {
+                const token = jwt.sign(
                     {userId: User._id},
                     "secretkey",
                     {expiresIn: "1h"}
                 )
-            })
-                .catch(error => res.status(500).json({error}))
-        }else{
-            console.error("Email ou mot de passe incorrect")
-            res.status(401).json({message: 'Email ou mot de passe incorect'})
+                res.status(200).json({
+                    userId: User._id,
+                    token: token
+                })
+            } else {
+                console.error("Email ou mot de passe incorrect")
+                res.status(401).json({message: 'Email ou mot de passe incorect'})
+            }
+        }catch (error){
+            res.status(500).json({message: error})
+            console.log(error)
         }
     }
 }
