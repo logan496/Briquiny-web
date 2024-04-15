cd123c4e2c4d4bfbcae640148ce3d842dabe418e
FOKO KENMOGNE Wilfried <143821047+FOKO-KENMOGNE-Wilfried@users.noreply.github.com>
2024-04-15 16:47:32 +0100

    Update is just a few detail about the briquiny_kotto page

--- briquiny-frontend/src/Components/Header/index.jsx
+++ briquiny-frontend/src/Components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import "./index.css"
 
 
@@ -9,7 +10,7 @@ function Header(){
         <div className="header">
             <h1 className="header-h1">BRIQUINY</h1>
             <div className="header_right">
-                
+                <NavLink to={"contact_us"} className="header_right_link">Contactez nous</NavLink>
             </div>
             <select className="header_language" name="language" id="language">
                 <option value="french">fr</option>
