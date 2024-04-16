d3212401c3425812735b665632cc26be44f81442
FOKO KENMOGNE Wilfried <143821047+FOKO-KENMOGNE-Wilfried@users.noreply.github.com>
2024-04-16 06:32:05 +0100

    I've update the header

--- briquiny-frontend/src/Components/Header/index.jsx
+++ briquiny-frontend/src/Components/Header/index.jsx
@@ -11,6 +11,9 @@ function Header(){
             <h1 className="header-h1">BRIQUINY</h1>
             <div className="header_right">
                 <NavLink to={"contact_us"} className="header_right_link">Contactez nous</NavLink>
+                <NavLink to={"/briquiny_kotto"} className="header_right_link">Briquiny Kotto</NavLink>
+                <NavLink to={"/briquiny_logbessou"} className="header_right_link">Briquiny Logbessou</NavLink>
+                <NavLink to={"/college_briquiny"} className="header_right_link">College Briquiny</NavLink>
             </div>
             <select className="header_language" name="language" id="language">
                 <option value="french">fr</option>
--- briquiny-frontend/src/main.jsx
+++ briquiny-frontend/src/main.jsx
@@ -7,7 +7,6 @@ import Footer from './Components/Footer/footer';
 import Briquiny_kotto from './pages/Briquiny_kotto';
 import Header from './Components/Header';
 import GlobalStyle from './utils/style/GlobalStyle';
-import Briquiny_logbessou from './pages/Briquiny_logbessou';
 import Briquiny_college from './pages/Briquiny_college/briquiny_college'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -19,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={ <Home /> } />
         <Route path='/contact_us' element={ <Contact_us /> } />
         <Route path='/briquiny_kotto' element={ <Briquiny_kotto /> } />
-        <Route path='/briquiny_logbessou' element={ <Briquiny_logbessou /> } />
+        <Route path='/briquiny_logbessou' element={ <Briquiny_kotto /> } />
         <Route path='/college_briquiny' element={ <Briquiny_college /> } />
       </Routes>
       <Footer/>
