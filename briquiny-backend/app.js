9b7f019615ec718da688c6d15c6a405d1eff9756
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-04-18 15:59:57 +0100

    corrections des bugs liés au cors

--- briquiny-backend/app.js
+++ briquiny-backend/app.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
 const router = require('./routes/router')
-require('./utils/dbconnector')
 
+require('./utils/dbconnector')
+app.use(cors ({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST',
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
-app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use('/briquiny', router)
 
+
 app.listen(3000, ()=>{
-    console.log('Serveur démarré sur le port 3000')
+    console.log('Serveur démarré')
 })
+
+
