7b3814a55b2aa38496920a9ec92fe6caa6b4ff8e
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-07-16 17:05:42 +0100

    modification de la gestion des prix de scolarité total -> tranches

--- briquiny-backend/middleware/addCycle.js
+++ briquiny-backend/middleware/addCycle.js
@@ -1,12 +1,13 @@
 //pour ajouter un nouveau cycle scolaire (sil, cp, CE1...)
 const Scolarite = require('../models/fraisScolaires')
 
-async function AddCycle(cycles, prix, prixInscription, etablissement){
+async function AddCycle(cycles, t1, t2, t3, prixInscription){
     const scolarite = new Scolarite({
         cycle: cycles,
-        montant: prix,
-        montantInscription: prixInscription,
-        ets: etablissement
+        tranche1 : t1,
+        tranche2 : t2,
+        tranche3 : t3,
+        montantInscription: prixInscription
     })
     await scolarite.save()
 }
