b821ce3e190a03c242208ebf68f0ce708e8489b5
logan496 <fils.tsajio@2027.ucac-icam.com>
2024-07-16 15:26:02 +0100

    modification de la gestion des prix de scolarité total -> tranches

--- briquiny-backend/controller/Create.js
+++ briquiny-backend/controller/Create.js
@@ -3,15 +3,10 @@ const {AddZone} = require("../middleware/AddZone")
 const Transport = require("../models/fraisTransport")
 class Create{
     static async addByCylce(req, res){
-        try{
-            const {cycles, prix, prixInscription, etablissement} =  req.body
-            await AddCycle(cycles, prix,prixInscription, etablissement)
-                .then(()=> res.status(200).json({message: 'cycle enregistré'}))
-                .catch(error => res.status(400).json({error}))
-        }catch (error){
-            res.status(500).json({error})
-        }
-
+        const {cycles, t1, t2, t3, prixInscription, etablissement} = req.body
+        await AddCycle(cycles, t1, t2, t3, prixInscription, etablissement)
+            .then(() => res.status(200).json({message: "cycle enregistré"}))
+            .catch(error => res.status(400).json({error}))
     }
     static async AddByZone(req, res){
         try{
